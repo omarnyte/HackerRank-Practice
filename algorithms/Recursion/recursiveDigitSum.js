94ade1ba85e3360cab4be95a72fae4529f8f7ecb
Omar <omar.dlsg@gmail.com>
2018-03-11 18:23:12 -0700

    Complete Recursive Digital Sum in JavaScript

---
+++ algorithms/Recursion/recursiveDigitSum.js
@@ -0,0 +1,68 @@
+// Given an integer, we need to find the super digit of the integer.
+
+// We define super digit of an integer  using the following rules:
+
+// If  has only  digit, then its super digit is.
+//     Otherwise, the super digit of  is equal to the super digit of the digit - sum of.Here, digit - sum of a number is defined as the sum of its digits.
+// For example, super digit of  will be calculated as:
+
+// super_digit(9875) = super_digit(9 + 8 + 7 + 5)
+//     = super_digit(29)
+//     = super_digit(2 + 9)
+//     = super_digit(11)
+//     = super_digit(1 + 1)
+//     = super_digit(2)
+//     = 2.
+// You are given two numbers  and.You have to calculate the super digit of.
+
+// is created when number  is concatenated  times.That is, if  and, then.
+
+// Input Format
+
+// The first line contains two space separated integers, and.
+
+//     Constraints
+
+// Output Format
+
+// Output the super digit of, where  is created as described above.
+
+// Sample Input 0
+
+// 148 3
+// Sample Output 0
+
+// 3
+// Explanation 0
+
+// Here  and, so.
+
+//     super_digit(P) = super_digit(148148148)
+//     = super_digit(1 + 4 + 8 + 1 + 4 + 8 + 1 + 4 + 8)
+//     = super_digit(39)
+//     = super_digit(3 + 9)
+//     = super_digit(12)
+//     = super_digit(1 + 2)
+//     = super_digit(3)
+//     = 3.
+
+function digitSum(n, k) {
+    let concatenated = "";
+
+    for (let i = 0; i < k; i++) {
+        concatenated += n.toString();
+    }
+
+    return superNum(parseInt(concatenated));
+}
+
+function superNum(num) {
+    if (num < 10) return num;
+
+    let sum = 0;
+    num.toString().split('').forEach(n => {
+        sum += parseInt(n);
+    })
+
+    return superNum(sum);
+}
